4cc7d61cf11b513e10356fdba116644c969f2dfe
D'juan Danilo Santos Cotia <160685852+DjuanDanilo@users.noreply.github.com>
2025-03-25 19:57:24 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,1 +1,62 @@
-
+let dados = [
+    {   
+        nome : "Explosão",
+        descricao : "Consome tudo que toques com chamas e brasa pura que a explosão causada por sua habilidade causa. Combina bem com pessoas de 'pavio curto'. (R$8.650,00)",
+        link : "",
+        imagem: "https://cdna.artstation.com/p/assets/images/images/011/597/148/original/anabela-faria-swirlball.gif?1530392157"  // Imagem genérica
+    },
+    {
+        nome : "Controle Molecular",
+        descricao : "Controle de moléculas que evolui conforme o tempo de uso. O poder é baseado completamente na sua compreensão da forma básica dos elementos, podendo materializar qualquer coisa em outra. Por exemplo, transformar carvão em diamante, recomendado! (R$18.800,00)",
+        link : "",
+        imagem: "https://media1.tenor.com/m/1f5ZOaYULS8AAAAd/samantha-eve-wilkins-invincible.gif"  // Imagem genérica
+    },
+    {
+        nome : "Água",
+        descricao : "O elemento água é fluido quanto moldável. Concede poder sobre todos seus estados e é muito versátil. Preço líquido (Pegou essa?) (R$13.900,00)",
+        link : "",
+        imagem: "https://comicvine.gamespot.com/a/uploads/original/11118/111183530/4483307-2153081175-korra.gif"  // Imagem genérica
+    },
+    {
+        nome : "Contrato",
+        descricao : "Nesses pergaminhos habita poderes ancestrais e feras de mitos que habitavam séculos passados demais para contar. Parece uma boa ideia adotar um deles! (R$20.000,00)",
+        link : "",
+        imagem: "https://i.pinimg.com/originals/7e/30/f6/7e30f6211a5f6964c45b1a7d222fff45.gif"  // Imagem genérica
+    },
+    {
+        nome : "Vento",
+        descricao : "O vento é etéreo e invisível, assim como pode ser silencioso e letal. Serve para aprender a assobiar e também te permite voar! (R$12.700,00)",
+        link : "",
+        imagem: "https://i.redd.it/otwwhz2hpbqb1.gif"  // Imagem genérica
+    },
+    {
+        nome : "Goma",
+        descricao : "A goma é tão flexível quanto elástica. Ideia totalmente original (Direitos reservados a Toei Animations, One Piece e HunterxHunter) (R$14.000,00)",
+        link : "",
+        imagem: "https://i.pinimg.com/originals/92/8b/ba/928bba6f8ba9548cffe95ea67da16aba.gif"  // Imagem genérica
+    },
+    {
+        nome : "Teleporte",
+        descricao : "Te permite viajar a distâncias incríveis, podendo levar mais alguém no trajeto. Te cansa mais do que esperar o Palmeiras ganhar um mundial. (R$12.000,00) ",
+        link : "",
+        imagem: "https://i.pinimg.com/originals/f0/b5/0c/f0b50c0d79730b998f1368c786d35807.gif"  // Imagem genérica
+    },
+    {
+        nome : "Energia",
+        descricao : "Te dará correntes elétricas que assumem uma aura energizada a sua volta. Para o uso, é recomendado uma forma de condutor tantos volts. Digamos que o preço é 'Chocante' (R$15.000,00)",
+        link : "https://i.pinimg.com/originals/66/a7/cf/66a7cf7ba06bec36385f23e77c04ad76.gif",
+        imagem: "https://i.pinimg.com/originals/66/a7/cf/66a7cf7ba06bec36385f23e77c04ad76.gif"  // Imagem genérica
+    },
+    {
+        nome : "Magia",
+        descricao : "Tal elemento arcano envolve forças misteriosas e ocultas. Costuma ser manipulado com auxílio de pedras rúnicas ou artefatos capazes de manipular a realidade de maneiras extraordinárias. (R$17.900,00)",
+        link : "",
+        imagem: "https://64.media.tumblr.com/0ba5fb864974d3802461f55673d59e44/5c1683c878920e20-da/s540x810/0bede0c3b6e9750dd0875213ff15e54bb0d8cee0.gif"  // Imagem genérica
+    },
+    {
+        nome : "Cristal",
+        descricao : "Te concede a capacidade majestosa de manipular monólitos relusentes que exalam preciosidade e poder em sua pura forma, e não se preocupe, isso se reflete perfeitamente no preço! (R$19.160,00)",
+        link : "",
+        imagem: "https://i.pinimg.com/originals/b5/42/ba/b542baab4e7be1821cc29df1f4020518.gif"  // Imagem genérica
+    }
+];
