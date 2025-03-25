4487d38133d48cafe61b92fa8f5b5178d0655c29
D'juan Danilo Santos Cotia <160685852+DjuanDanilo@users.noreply.github.com>
2025-03-25 19:58:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+function gerarResultados(dados) {
+    let section = document.getElementById('resultados-pesquisa');
+    let resultados = "";
 
+    for (let dado of dados) {
+        resultados += `
+            <div class="item-resultado">
+                <img src="${dado.imagem}" alt="${dado.nome}" class="imagem-produto">
+                <h2><a href="${dado.link}" target="_blank">${dado.nome}</a></h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href="${dado.link}" target="_blank" rel="noopener noreferrer">Mais Informações</a>
+            </div>
+        `;
+    }
+
+    if (!resultados) {
+        resultados = "<p>Resultado não Encontrado</p>";
+    }
+
+    section.innerHTML = resultados;
+}
+
+function pesquisar() {
+    let inputElement = document.querySelector("input");
+    let campoPesquisa = inputElement.value.toLowerCase();
+
+    let dadosFiltrados = dados.filter(dado => 
+        dado.nome.toLowerCase().includes(campoPesquisa) || 
+        dado.descricao.toLowerCase().includes(campoPesquisa)
+    );
+
+    gerarResultados(dadosFiltrados);
+}
+
+gerarResultados(dados);
+
+document.querySelector("input").addEventListener("input", pesquisar);
